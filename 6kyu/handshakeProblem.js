13b7153403ef4384d1b4be0b033fa87387212fcb
David Yu <dyu5286@gmail.com>
2023-08-21 12:13:34 -0400

    codewars82123

---
+++ 6kyu/handshakeProblem.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/5574835e3e404a0bed00001b/train/javascript
+
+// Johnny is a farmer and he annually holds a beet farmers convention "Drop the beet".
+
+// Every year he takes photos of farmers handshaking. Johnny knows that no two farmers handshake more than once. He also knows that some of the possible handshake combinations may not happen.
+
+// However, Johnny would like to know the minimal amount of people that participated this year just by counting all the handshakes.
+
+// Help Johnny by writing a function, that takes the amount of handshakes and returns the minimal amount of people needed to perform these handshakes (a pair of farmers handshake only once).
+
+function getParticipants(handshakes){
+  return 0
+}
+
+console.log(getParticipants(0))//, 0, "0 handshakes")
+console.log(getParticipants(1))//, 2, "1 handshake")
+console.log(getParticipants(3))//, 3, "3 handshakes")
+console.log(getParticipants(6))//, 4, "6 handshakes")
+console.log(getParticipants(7))//, 5, "7 handshakes")
