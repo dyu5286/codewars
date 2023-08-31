144bc747a3129f131c36046a24bbb6416c76d007
David Yu <dyu5286@gmail.com>
2023-08-31 15:15:57 -0400

    codewars83123

---
+++ 6kyu/persistentBugger.js
@@ -0,0 +1,36 @@
+//https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+//Input is a number
+//Output is a number
+//declare a variable, n representing the number of times you must multiply
+//declare a function, product which is the function to multiply all integers in a number
+//conditional for if num < 10
+//else, while loop that uses the product function and also n++
+//return n
+
+function persistence(num) {
+   let n = 0
+   while(num > 9){
+            num = multiply(num)
+            n++
+    }
+    return n
+}
+
+function multiply(number){
+    let numArr = number.toString().split("").map(Number).reduce((a,b) => a*b, 1)
+    return numArr
+}
+
+console.log(persistence(39))//,3);
+console.log(persistence(4))//,0);
+console.log(persistence(25))//,2);
+console.log(persistence(999))//,4);
