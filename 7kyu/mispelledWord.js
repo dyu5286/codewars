3bcf475f637fc3574e02e3572b70031c92d0503f
David Yu <dyu5286@gmail.com>
2023-10-14 11:49:16 -0400

    codewars101423

---
+++ 7kyu/mispelledWord.js
@@ -0,0 +1,40 @@
+//https://www.codewars.com/kata/5892595f190ca40ad0000095/train/javascript
+
+// Create a function mispelled(word1, word2):
+
+// mispelled('versed', 'xersed'); // returns true
+// mispelled('versed', 'applb'); // returns false
+// mispelled('versed', 'v5rsed'); // returns true
+// mispelled('1versed', 'versed'); // returns true
+// mispelled('versed', 'versed'); // returns true
+// It checks if the word2 differs from word1 by at most one character.
+
+// This can include an extra char at the end or the beginning of either of words.
+
+// In the tests that expect true, the mispelled word will always differ mostly by one character. If the two words are the same, return True.
+
+var mispelled = (word1, word2) => {
+    if(Math.abs(word1.length - word2.length) < 2){
+        if(word1.includes(word2) || word2.includes(word1)){
+            return true
+        }else{
+        let dismatch = 0;
+        for (let i in word1) {
+          if(word1[i] !== word2[i]){
+                dismatch++;
+           }
+          } 
+          return dismatch < 2
+        }
+    } return false
+}
+
+console.log(mispelled('versed', 'xersed'))//, true);
+console.log(mispelled('versed', 'applb'))//, false);
+console.log(mispelled('versed', 'v5rsed'))//, true);
+console.log(mispelled('1versed', 'versed'))//, true);
+console.log(mispelled('versed', 'versed1'))//, true);
+console.log(mispelled('versed', 'aversed'))//, true);
+console.log(mispelled('aaversed', 'versed'))//, false);
+console.log(mispelled('versed', 'aaversed'))//, false);
+
