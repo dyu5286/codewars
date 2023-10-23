fbe1186ebe0597c04e67e73d1bd13fb9371e8795
David Yu <dyu5286@gmail.com>
2023-10-23 16:03:54 -0400

    codewars102323

---
+++ 8kyu/squaringAnArgument.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+//Now you have to write a function that takes an argument and returns the square of it.
+
+function square(x){
+    return x * x
+}
