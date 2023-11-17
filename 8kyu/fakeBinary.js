aaf78cdd5d9e7a0da0a4b67a267400cadd184314
David Yu <dyu5286@gmail.com>
2023-11-17 13:15:48 -0500

    codewars111723

---
+++ 8kyu/fakeBinary.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+function fakeBin(x){
+    return x.split("").map(a => a < 5 ? 0 :1).join("")
+}
