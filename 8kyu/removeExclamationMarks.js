0ed2d39a34d6e13c06a11fcbe0ae9884ca92702d
David Yu <dyu5286@gmail.com>
2023-12-02 10:33:56 -0500

    codewars12223

---
+++ 8kyu/removeExclamationMarks.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/57fae964d80daa229d000126/train/javascript
+
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+function remove (string) {
+    let stringArr = string.split("")
+    stringArr[stringArr.length - 1] === "!" ? stringArr.pop() : null
+    return stringArr.join("")
+}
