9d9607a08a3d1e6dc17123e1c44804583db0b015
David Yu <dyu5286@gmail.com>
2023-12-14 17:04:39 -0500

    codewars121423

---
+++ 8kyu/returningStrings.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
+
+console.log(greet("Ryan"))//, "Hello, Ryan how are you doing today?");
+console.log(greet("Shingles"))//, "Hello, Shingles how are you doing today?");
