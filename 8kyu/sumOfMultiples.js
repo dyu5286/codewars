407f313b2f0f2aa0528327e3564c07b00a07e8f3
David Yu <dyu5286@gmail.com>
2023-12-15 18:09:24 -0500

    codewars121523

---
+++ 8kyu/sumOfMultiples.js
@@ -0,0 +1,30 @@
+//https://www.codewars.com/kata/57241e0f440cd279b5000829/train/javascript
+
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+function sumMul(n,m){
+    let total = 0
+    let i = n
+    if(n > m || n === m){
+        return "INVALID"
+    }else{
+        while(i < m){
+            total += i
+            i += n
+        }
+    }
+    return total
+}
+
+console.log(sumMul(0,0))//,"INVALID");
+console.log(sumMul(2,9))//,20);
+console.log(sumMul(4,-7))//,"INVALID");
