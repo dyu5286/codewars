8e742152b4abdfd112f2702ea99fa91d201fe638
David Yu <dyu5286@gmail.com>
2024-01-23 11:56:36 -0500

    codewars12324

---
+++ 8kyu/removeDuplicatesFromList.js
@@ -0,0 +1,25 @@
+//https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/javascript
+
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+function distinct(a) {
+  let outputArr = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!outputArr.includes(a[i])) {
+      outputArr.push(a[i]);
+    }
+  }
+  return outputArr;
+}
+
+console.log(distinct([1])); //, [1]);
+console.log(distinct([1, 2])); //, [1, 2]);
+console.log(distinct([1, 1, 2])); //, [1, 2]);
