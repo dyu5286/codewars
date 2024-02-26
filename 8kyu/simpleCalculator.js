7c16746fd51c3bc699d54fafe84cbffeedc2f0d3
David Yu <dyu5286@gmail.com>
2024-02-26 14:07:53 -0500

    codewars22624

---
+++ 8kyu/simpleCalculator.js
@@ -0,0 +1,36 @@
+//https://www.codewars.com/kata/5810085c533d69f4980001cf/train/javascript
+
+// You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+// Your function will accept three arguments:
+// The first and second argument should be numbers.
+// The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+// if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+
+// Example:
+// calculator(1,2,"+"); //=> result will be 3
+// calculator(1,2,"&"); //=> result will be "unknown value"
+// calculator(1,"k","*"); //=> result will be "unknown value"
+
+function calculator(a, b, sign) {
+    if(sign === "+"){
+        return a + b
+    }else if(sign === "-"){
+        return a - b
+    }else if(sign === "*"){
+        return a * b
+    }else if(sign === "/"){
+        return a / b
+    }else{
+        return "unknown value"
+    }
+
+}
+
+console.log(calculator(1, 2, "+"))//, 3, "calculate");
+console.log(calculator(1, 2, "-"))//, -1, "calculate");
+console.log(calculator(3, 5, "*"))//, 15, "calculate");
+console.log(calculator(6, 2, "/"))//, 3, "calculate");
+console.log(calculator(6, 2, "$"))//, "unknown value", "calculate");
+console.log(calculator(6, "h", "*"))//, "unknown value", "calculate");  
